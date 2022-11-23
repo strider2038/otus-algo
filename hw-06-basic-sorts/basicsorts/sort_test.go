e145c4b9eab1f818c015d82e94ca04d896b9c606
Igor Lazarev <strider2038@yandex.ru>
2022-11-23 11:38:59 +0300

    sorts

--- hw-06-basic-sorts/basicsorts/sort_test.go
+++ hw-06-basic-sorts/basicsorts/sort_test.go
@@ -1,10 +1,78 @@
 package basicsorts_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/strider2038/otus-algo/datatesting"
 	"github.com/strider2038/otus-algo/hw-06-basic-sorts/basicsorts"
 )
+
+type Solver func(items []int) []int
+
+func (s Solver) Solve(t *testing.T, input, output []string) {
+	if len(input) < 2 || len(output) < 1 {
+		t.Fatal(datatesting.ErrNotEnoughArguments)
+	}
+
+	unsorted, err := datatesting.ParseIntArray(input[1])
+	if err != nil {
+		t.Fatalf("parse input array: %v", err)
+	}
+	wantSorted, err := datatesting.ParseIntArray(output[0])
+	if err != nil {
+		t.Fatalf("parse output array: %v", err)
+	}
+
+	start := time.Now()
+	gotSorted := s(unsorted)
+	t.Log("elapsed time:", time.Since(start).String())
+
+	datatesting.AssertEqualArrays(t, wantSorted, gotSorted)
+}
+
+func TestSortTable(t *testing.T) {
+	tests := []struct {
+		name string
+		sort func(items []int) []int
+	}{
+		{
+			name: "bubble",
+			sort: basicsorts.Bubble[int],
+		},
+		{
+			name: "insertion",
+			sort: basicsorts.Insertion[int],
+		},
+		{
+			name: "shell",
+			sort: basicsorts.Shell[int],
+		},
+	}
+	arrayTypes := []struct {
+		name string
+		dir  string
+	}{
+		{name: "random", dir: "0.random"},
+		{name: "digits", dir: "1.digits"},
+		{name: "sorted", dir: "2.sorted"},
+		{name: "reverse", dir: "3.revers"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			for _, arrayType := range arrayTypes {
+				t.Run(arrayType.name, func(t *testing.T) {
+					runner := datatesting.NewRunner(
+						datatesting.WithWorkdir(fmt.Sprintf("./../../testdata/sortdata/%s/", arrayType.dir)),
+						datatesting.WithLimit(5),
+					)
+					runner.Run(t, Solver(test.sort))
+				})
+			}
+		})
+	}
+}
 
 func TestSort(t *testing.T) {
 	tests := []struct {
