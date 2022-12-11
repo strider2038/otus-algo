3cd16547c94b6a864434a6dd6b039ae52a68b132
Igor Lazarev <strider2038@yandex.ru>
2022-12-11 16:54:12 +0300

    HW-10 search trees

---
+++ hw-10-search-trees/avl/tree.go
@@ -0,0 +1,221 @@
+package avl
+
+import "errors"
+
+var ErrNotFound = errors.New("not found")
+
+type Value interface {
+	~string |
+		~float32 | ~float64 |
+		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type Tree[V Value] struct {
+	root *Node[V]
+}
+
+func (tree *Tree[V]) Find(value V) (*Node[V], error) {
+	node := tree.root.find(value)
+	if node == nil {
+		return nil, ErrNotFound
+	}
+
+	return node, nil
+}
+
+func (tree *Tree[V]) Insert(value V) {
+	tree.root = tree.root.insert(value)
+}
+
+func (tree *Tree[V]) Remove(value V) {
+	tree.root = tree.root.remove(value)
+}
+
+func (tree *Tree[V]) ForEach(f func(value V) error) error {
+	return tree.root.forEach(f)
+}
+
+// Методы к реализации:
+// smallLeftRotation(Tree t) smallRightRotation(Tree t) - малое левое/правое вращение
+// bigLeftRotation(Tree t), bigRightRotation(Tree t) - большое левое/правое вращение, написать через вызов малых вращений
+// (при желании выполнить оптимизацию: переписать через прямые присваивания и проверить эффективность)
+// void insert(int x) - вставка элемента
+// void remove(int x) - удаление элемента
+// void rebalance(Tree t) - перебалансировка дерева
+
+type Node[V Value] struct {
+	Value  V
+	height int
+	left   *Node[V]
+	right  *Node[V]
+}
+
+func (node *Node[V]) find(value V) *Node[V] {
+	if node == nil {
+		return nil
+	}
+	if value == node.Value {
+		return node
+	}
+	if value < node.Value {
+		return node.left.find(value)
+	}
+
+	return node.right.find(value)
+}
+
+func (node *Node[V]) insert(value V) *Node[V] {
+	if node == nil {
+		return &Node[V]{Value: value, height: 1}
+	}
+
+	if value <= node.Value {
+		node.left = node.left.insert(value)
+	} else {
+		node.right = node.right.insert(value)
+	}
+
+	return node.rebalance()
+}
+
+func (node *Node[V]) remove(value V) *Node[V] {
+	if node == nil {
+		return nil
+	}
+
+	if value < node.Value { // значение меньше текущего - идем влево
+		node.left = node.left.remove(value)
+	} else if value > node.Value { // значение больше текущего - идем вправо
+		node.right = node.right.remove(value)
+		// искомый узел найден
+	} else if node.left == nil && node.right == nil {
+		// узел без детей - возвращаем nil (на вызывающей стороне ссылка будет стерта)
+		node = nil
+	} else if node.left == nil { // левого узла нет - возвращаем правый узел
+		node = node.right
+	} else if node.right == nil { // правого узла нет - возвращаем левый узел
+		node = node.left
+	} else {
+		// ищем наименьший узел в правой части дерева
+		smallestOnRight := node.right
+		for smallestOnRight != nil && smallestOnRight.left != nil {
+			smallestOnRight = smallestOnRight.left
+		}
+
+		// заменяем текущий узел наименьшим из правой части
+		node.Value = smallestOnRight.Value
+		// удаляем наименьший узел из правой части
+		node.right = node.right.remove(node.Value)
+	}
+
+	return node.rebalance()
+}
+
+func (node *Node[V]) forEach(f func(value V) error) error {
+	if node == nil {
+		return nil
+	}
+	if err := node.left.forEach(f); err != nil {
+		return err
+	}
+	if err := f(node.Value); err != nil {
+		return err
+	}
+	if err := node.right.forEach(f); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (node *Node[V]) rebalance() *Node[V] {
+	if node == nil {
+		return nil
+	}
+
+	node.recalculateHeight()
+
+	// проверка баланса дерева
+	balance := node.left.getHeight() - node.right.getHeight()
+
+	// вращаем влево, если высота левого поддерева больше правого
+	if balance <= -2 {
+		// если высота левого поддерева больше правого, то
+		// выполняем большой поворот влево
+		if node.right.left.getHeight() > node.right.right.getHeight() {
+			return node.bigRotateLeft()
+		}
+
+		return node.smallRotateLeft()
+	}
+
+	if balance >= 2 {
+		// если высота правого поддерева больше левого, то
+		// выполняем большой поворот вправо
+		if node.left.right.getHeight() > node.left.left.getHeight() {
+			return node.bigRotateRight()
+		}
+
+		return node.smallRotateRight()
+	}
+
+	return node
+}
+
+func (node *Node[V]) bigRotateLeft() *Node[V] {
+	node.right = node.right.smallRotateRight()
+
+	return node.smallRotateLeft()
+}
+
+func (node *Node[V]) bigRotateRight() *Node[V] {
+	node.left = node.left.smallRotateLeft()
+
+	return node.smallRotateRight()
+}
+
+func (node *Node[V]) smallRotateLeft() *Node[V] {
+	newRoot := node.right
+	node.right = newRoot.left
+	newRoot.left = node
+
+	node.recalculateHeight()
+	newRoot.recalculateHeight()
+
+	return newRoot
+}
+
+func (node *Node[V]) smallRotateRight() *Node[V] {
+	newRoot := node.left
+	node.left = newRoot.right
+	newRoot.right = node
+
+	node.recalculateHeight()
+	newRoot.recalculateHeight()
+
+	return newRoot
+}
+
+func (node *Node[V]) recalculateHeight() {
+	if node == nil {
+		return
+	}
+
+	leftHeight := node.left.getHeight()
+	rightHeight := node.right.getHeight()
+
+	if leftHeight > rightHeight {
+		node.height = leftHeight + 1
+	} else {
+		node.height = rightHeight + 1
+	}
+}
+
+func (node *Node[V]) getHeight() int {
+	if node == nil {
+		return 0
+	}
+
+	return node.height
+}
---
+++ hw-10-search-trees/avl/tree_test.go
@@ -0,0 +1,171 @@
+package avl_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/strider2038/otus-algo/datatesting"
+	"github.com/strider2038/otus-algo/hw-08-quick-external-sorts/sort"
+	"github.com/strider2038/otus-algo/hw-10-search-trees/avl"
+)
+
+func TestTree_Insert(t *testing.T) {
+	tree := avl.Tree[int]{}
+
+	tree.Insert(5)
+	tree.Insert(8)
+	tree.Insert(3)
+	tree.Insert(1)
+	tree.Insert(11)
+	tree.Insert(7)
+
+	got := make([]int, 0)
+	tree.ForEach(func(value int) error {
+		got = append(got, value)
+		return nil
+	})
+	datatesting.AssertEqualArrays(t, []int{1, 3, 5, 7, 8, 11}, got)
+}
+
+func TestTree_Remove(t *testing.T) {
+	tests := []struct {
+		name   string
+		values []int
+		remove int
+		want   []int
+	}{
+		{
+			name:   "remove not existing node",
+			values: []int{10, 3, 5, 15, 13, 11},
+			remove: -1,
+			want:   []int{3, 5, 10, 11, 13, 15},
+		},
+		{
+			name:   "remove root node",
+			values: []int{2, 1, 3},
+			remove: 2,
+			want:   []int{1, 3},
+		},
+		{
+			name:   "remove left node without children",
+			values: []int{2, 1, 3},
+			remove: 1,
+			want:   []int{2, 3},
+		},
+		{
+			name:   "remove right node without children",
+			values: []int{2, 1, 3},
+			remove: 3,
+			want:   []int{1, 2},
+		},
+		{
+			name:   "remove node with right child",
+			values: []int{4, 2, 3},
+			remove: 2,
+			want:   []int{3, 4},
+		},
+		{
+			name:   "remove node with left child",
+			values: []int{4, 2, 1},
+			remove: 2,
+			want:   []int{1, 4},
+		},
+		{
+			name:   "remove node with both children",
+			values: []int{5, 2, 4, 1, 3},
+			remove: 2,
+			want:   []int{1, 3, 4, 5},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tree := createTreeFromNumbers(test.values)
+
+			tree.Remove(test.remove)
+
+			got := make([]int, 0)
+			tree.ForEach(func(value int) error {
+				got = append(got, value)
+				return nil
+			})
+			datatesting.AssertEqualArrays(t, test.want, got)
+		})
+	}
+}
+
+func TestTree(t *testing.T) {
+	treeOperations := []struct {
+		name string
+		do   func(tree *avl.Tree[int], values []int) error
+	}{
+		{
+			name: "find",
+			do: func(tree *avl.Tree[int], values []int) error {
+				value := values[rand.Intn(len(values))]
+				_, err := tree.Find(value)
+				return err
+			},
+		},
+		{
+			name: "remove",
+			do: func(tree *avl.Tree[int], values []int) error {
+				tree.Remove(values[rand.Intn(len(values))])
+				return nil
+			},
+		},
+	}
+
+	counts := []int{1000, 10_000, 100_000}
+
+	for _, operation := range treeOperations {
+		for _, count := range counts {
+			numbers := GenerateRandomNumbers(count, count)
+
+			t.Run(fmt.Sprintf("%s, n=%d, random", operation.name, count), func(t *testing.T) {
+				tree := createTreeFromNumbers(numbers)
+
+				start := time.Now()
+				for i := 0; i < count/10; i++ {
+					if err := operation.do(tree, numbers); err != nil {
+						t.Fatal(err)
+					}
+				}
+				t.Log("elapsed time:", time.Since(start).String())
+			})
+
+			numbers = sort.QuickMiddle(numbers)
+			t.Run(fmt.Sprintf("%s, n=%d, sorted", operation.name, count), func(t *testing.T) {
+				tree := createTreeFromNumbers(numbers)
+
+				start := time.Now()
+				for i := 0; i < count/10; i++ {
+					if err := operation.do(tree, numbers); err != nil {
+						t.Error(err)
+					}
+				}
+				t.Log("elapsed time:", time.Since(start).String())
+			})
+		}
+	}
+}
+
+func GenerateRandomNumbers(n int, max int) []int {
+	rand.Seed(time.Now().UnixNano())
+
+	numbers := make([]int, n)
+	for i := 0; i < n; i++ {
+		numbers[i] = rand.Intn(max)
+	}
+
+	return numbers
+}
+
+func createTreeFromNumbers(values []int) *avl.Tree[int] {
+	tree := &avl.Tree[int]{}
+	for _, value := range values {
+		tree.Insert(value)
+	}
+	return tree
+}
