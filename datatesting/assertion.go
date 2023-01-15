200a55d7410bed32b17f03537373cfb3c4c35019
Igor Lazarev <strider2038@yandex.ru>
2023-01-15 11:24:04 +0300

    datatesting updated

--- datatesting/assertion.go
+++ datatesting/assertion.go
@@ -14,6 +14,20 @@ func AssertEqual[T comparable](t *testing.T, want, got T) {
 	}
 }
 
+func AssertTrue(t *testing.T, isTrue bool) {
+	t.Helper()
+	if !isTrue {
+		t.Errorf("test failed: want true, got false")
+	}
+}
+
+func AssertFalse(t *testing.T, isTrue bool) {
+	t.Helper()
+	if isTrue {
+		t.Errorf("test failed: want false, got true")
+	}
+}
+
 func AssertEqualArrays(t *testing.T, wantItems []int, got []int) {
 	t.Helper()
 
@@ -31,6 +45,37 @@ func AssertEqualArrays(t *testing.T, wantItems []int, got []int) {
 			if errsCount > 100 {
 				t.Errorf("too much errors")
 				return
+			}
+		}
+	}
+}
+
+func AssertEqualMatrix(t *testing.T, want [][]int, got [][]int) {
+	t.Helper()
+
+	if len(want) != len(got) {
+		t.Errorf("different matrix rows count: want %d, got %d", len(want), len(got))
+		return
+	}
+
+	for i := 0; i < len(want); i++ {
+		wantRow := want[i]
+		gotRow := got[i]
+
+		if len(wantRow) != len(gotRow) {
+			t.Errorf("different length at row %d: want %d, got %d", i, len(wantRow), len(gotRow))
+			continue
+		}
+
+		errsCount := 0
+		for j := 0; j < len(wantRow); j++ {
+			if wantRow[j] != gotRow[j] {
+				t.Errorf("different items at [%d][%d]: want %d, got %d", i, j, wantRow[j], gotRow[j])
+				errsCount++
+				if errsCount > 100 {
+					t.Errorf("too much errors")
+					return
+				}
 			}
 		}
 	}
